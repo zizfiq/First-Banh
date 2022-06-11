e9972536df1c44e40f9d8215243ab1206613ffd7
fiqri.aaziz <107309661+zizfiq@users.noreply.github.com>
2022-06-11 23:20:09 +0700

    Tugas java
    pemograman lanjut

---
+++ KasusB.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class KasusB {
+    //FiqriAbdulAziz - 6304211405
+    public static void main(String[] args) {
+        float f=34.1234f;
+        int i=10;
+        double d=14.0;
+
+        System.out.println("Nilai f adalah "+f);
+        System.out.println("Nilai f adalah "+i);
+        System.out.println("Nilai f adalah "+d);
+    }
+}
